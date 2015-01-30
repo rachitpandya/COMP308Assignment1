f5d9620285c43c9e486056a657628381f2144614
rachitpandya <pandyarachit007@gmail.com>
2015-01-30 02:53:29 -0500

    Commit After creating Services and ContactMe page

--- Assignment1EmergingTech/Scripts/lib/app.js
+++ Assignment1EmergingTech/Scripts/lib/app.js
@@ -1,1 +1,11 @@
-﻿
+﻿myfunction()
+{
+    var name = document.getElementById("nm");
+    var emailid = document.getElementById("email");
+    var msg=document.getElementById("msg");
+        if (name=="")
+    {
+            alert("Name can not be empty");
+            
+    }
+}
