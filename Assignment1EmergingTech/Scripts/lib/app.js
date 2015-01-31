066e167187a6b0cf23fa1269748bbfb8f09599de
rachitpandya <pandyarachit007@gmail.com>
2015-01-30 23:15:41 -0500

    Commit after Final Changes in my portfolio

--- Assignment1EmergingTech/Scripts/lib/app.js
+++ Assignment1EmergingTech/Scripts/lib/app.js
@@ -1,11 +1,1 @@
-﻿myfunction()
-{
-    var name = document.getElementById("nm");
-    var emailid = document.getElementById("email");
-    var msg=document.getElementById("msg");
-        if (name=="")
-    {
-            alert("Name can not be empty");
-            
-    }
-}
+﻿
